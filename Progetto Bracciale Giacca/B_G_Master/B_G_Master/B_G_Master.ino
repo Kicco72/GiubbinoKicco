7e26a6bd321329fde82e7ff864beb73eaad48084
Kicco972 <francescobevilacqua7@gmail.com>
2025-12-04 22:06:33 +0100

    inpostazione librerie Arduino Giga R1 WiFi

--- Progetto Bracciale Giacca/B_G_Master/B_G_Master/B_G_Master.ino
+++ Progetto Bracciale Giacca/B_G_Master/B_G_Master/B_G_Master.ino
@@ -1,20 +1,24 @@
 
 // Librerie
-
-#include <WiFi.h>                    // WiFi
 #include "mbed.h"                    // Mbed OS
+#include <WiFi.h>                    // WiFi
+#include <ArduinoBLE.h>              // Bluetooth® Low Energy
 #include <Arduino_GigaDisplay.h>     // RGB Led
 #include "Arduino_GigaDisplay_GFX.h" // Display
 #include "Arduino_BMI270_BMM150.h"   // IMU
-#include "Arduino_H7_Video.h"        //IMU
-#include "lvgl.h"                    //IMU
+#include "Arduino_H7_Video.h"        // IMU
+#include "lvgl.h"                    // IMU
+#include "AdvancedDAC.h"             // DAC Avanzato audio
+#include "PluggableUSBHID.h"         // USB HID
+#include "USBKeyboard.h"             // USB Keyboard
+
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // Inpostazioni iniziali
+   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Ciclo principale
 
 }
