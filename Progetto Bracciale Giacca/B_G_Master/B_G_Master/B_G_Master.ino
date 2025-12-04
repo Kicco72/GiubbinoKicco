d03d4895bf1312a29816ecb331ffa0be651179ad
Kicco972 <francescobevilacqua7@gmail.com>
2025-12-04 21:47:59 +0100

    Start Commit

---
+++ Progetto Bracciale Giacca/B_G_Master/B_G_Master/B_G_Master.ino
@@ -0,0 +1,20 @@
+
+// Librerie
+
+#include <WiFi.h>                    // WiFi
+#include "mbed.h"                    // Mbed OS
+#include <Arduino_GigaDisplay.h>     // RGB Led
+#include "Arduino_GigaDisplay_GFX.h" // Display
+#include "Arduino_BMI270_BMM150.h"   // IMU
+#include "Arduino_H7_Video.h"        //IMU
+#include "lvgl.h"                    //IMU
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
